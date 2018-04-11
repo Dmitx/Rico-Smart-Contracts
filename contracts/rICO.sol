ae0bf13555fbc01b4bc118657f1e1ed58b41ed46
Dmitx <37021407+Dmitx@users.noreply.github.com>
2018-04-11 23:29:42 +0300

    Remove duplicate ReentrancyGuard contract from rICO

--- contracts/rICO.sol
+++ contracts/rICO.sol
@@ -4,30 +4,6 @@ import "./RicoToken.sol";
 import "./PreSale.sol";
 
 
-contract ReentrancyGuard {
-
-    /**
-     * @dev We use a single lock for the whole contract.
-     */
-    bool private reentrancy_lock = false;
-
-    /**
-     * @dev Prevents a contract from calling itself, directly or indirectly.
-     * @notice If you mark a function `nonReentrant`, you should also
-     * mark it `external`. Calling one nonReentrant function from
-     * another is not supported. Instead, you can implement a
-     * `private` function doing the actual work, and a `external`
-     * wrapper marked as `nonReentrant`.
-     */
-    modifier nonReentrant() {
-        require(!reentrancy_lock);
-        reentrancy_lock = true;
-        _;
-        reentrancy_lock = false;
-    }
-
-}
-
 contract rICO is Ownable, ReentrancyGuard {
     using SafeMath for uint256;
 
