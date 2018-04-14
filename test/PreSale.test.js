8a011c0d7146371f15de86eca80e9ce99488cf48
Dmitx <37021407+Dmitx@users.noreply.github.com>
2018-04-15 02:03:39 +0300

    Add PreSale tests

--- test/PreSale.test.js
+++ test/PreSale.test.js
@@ -1,158 +1,140 @@
+import ether from './helpers/ether';
+import {advanceBlock} from './helpers/advanceToBlock';
+import {increaseTimeTo, duration} from './helpers/increaseTime';
+import latestTime from './helpers/latestTime';
+import EVMRevert from './helpers/EVMRevert';
+
 const BigNumber = web3.BigNumber;
 
+require('chai')
+    .use(require('chai-as-promised'))
+    .use(require('chai-bignumber')(BigNumber))
+    .should();
 
-const Token = artifacts.require('./RicoToken.sol');
-const Presale = artifacts.require("./PreSale.sol");
+var chai = require('chai');
+var assert = chai.assert;
 
+const PreSaleInterface = artifacts.require('./PreSale.sol');
+const TokenInterface = artifacts.require('./RicoToken.sol');
 
-contract('PreSale', function (accounts) {
+contract('PreSaleTest', function ([_, owner, investor, wallet, purchaser]) {
+    const value = ether(42);
 
-    it("token's admin should be PreSale Contract", async () => {
-        let TokenInstance = await Token.deployed();
+    before(async function () {
+        // Advance to the next block to correctly read time in the solidity "now" function interpreted by ganache
+        await advanceBlock();
+    });
 
-        // unnecessary because add PreSale to admin in migrations
-        // await TokenInstance.addAdmin(Presale.address);
+    beforeEach(async function () {
+        this.openingTime = latestTime() + duration.weeks(1);
+        this.periodTimeInDays = 30;
+        this.afterClosingTime = this.openingTime + duration.days(this.periodTimeInDays) + duration.seconds(1);
+        this.token = await TokenInterface.new();
+        this.minimumInvest = ether(15);
+        this.preSale = await PreSaleInterface.new(this.openingTime, this.periodTimeInDays, wallet, this.token.address, this.minimumInvest);
+    });
 
-        let isAdmin = await TokenInstance.isAdmin(Presale.address);
-        console.log(isAdmin);
-        assert.equal(true, isAdmin);
-    })
+    describe('right deploy PreSale contract', function () {
 
-    // it("whiteList should work correct", async () => {
-    //     let TokenInstance = await Token.deployed();
-    //     let PresaleInstance = await Presale.deployed();
-    //     let WhiteListInstance = await WhiteList.deployed();
-    //     let PresaleOwner = await PresaleInstance.owner.call();
-    //     let WLOwner = await WhiteListInstance.owner.call();
-    //
-    //     assert.equal(accounts[0], PresaleOwner, "Presale's Owner");
-    //     assert.equal(accounts[0], WLOwner, "WhiteList's Owner");
-    //
-    //     try {
-    //         await PresaleInstance.sendTransaction({ value: 1e+18, from: accounts[1] })
-    //         assert.fail()
-    //     } catch (error) {
-    //         err = error
-    //     }
-    //     assert.ok(err instanceof Error)
-    //     console.log(err)
-    //
-    //     let tokenBalance = await TokenInstance.balanceOf(accounts[1]).then(result => result.toNumber());
-    //     let tokenBalance1 = tokenBalance;
-    //     assert.equal(tokenBalance1, 0, "should be zero");
-    //
-    //     console.log(tokenBalance1)
-    //
-    //     await WhiteListInstance.addToWhiteList(accounts[1])
-    //
-    //     let inWL = await WhiteListInstance.isInWhiteList(accounts[1]);
-    //
-    //     assert.equal(inWL, true, "should be true");
-    //
-    //     await PresaleInstance.sendTransaction({ value: 1e+18, from: accounts[1] })
-    //
-    //     tokenBalance = await TokenInstance.balanceOf(accounts[1]);
-    //     let tokenBalance2 = tokenBalance.toNumber();
-    //     console.log(tokenBalance2)
-    // })
-    //
-    // it("token price can be changed only oracle", async () => {
-    //     let TokenInstance = await Token.deployed();
-    //     let PresaleInstance = await Presale.deployed();
-    //     let WhiteListInstance = await WhiteList.deployed();
-    //
-    //     try {
-    //         await PresaleInstance.changePriceUSD(14692121690100,{from: accounts[1]})
-    //         assert.fail()
-    //     } catch (error) {
-    //         err = error
-    //     }
-    //     assert.ok(err instanceof Error)
-    //     console.log(err)
-    //
-    //     try {
-    //         await PresaleInstance.setOracle(accounts[1], {from: accounts[1]})
-    //         assert.fail()
-    //     } catch (error) {
-    //         err = error
-    //     }
-    //     assert.ok(err instanceof Error)
-    //     console.log(err)
-    //
-    //
-    //     await PresaleInstance.setOracle(accounts[1], {from: accounts[0]})
-    //
-    //     await PresaleInstance.changePriceUSD(14692121690100,{from: accounts[1]})
-    //
-    //     let priceUSD = await PresaleInstance.priceUSD.call().then(result => result.toNumber());
-    //
-    //     assert.equal(priceUSD,14692121690100 , "priceUSD");
-    // })
-    //
-    //
-    // it("manual transfer should work correct", async () => {
-    //     let TokenInstance = await Token.deployed();
-    //     let PresaleInstance = await Presale.deployed();
-    //     let WhiteListInstance = await WhiteList.deployed();
-    //
-    //     try {
-    //         await PresaleInstance.manualTransfer(accounts[1], 1000, { from: accounts[1] })
-    //         assert.fail()
-    //     } catch (error) {
-    //         err = error
-    //     }
-    //     assert.ok(err instanceof Error)
-    //     console.log(err)
-    //
-    //     // manual transfer can be called only from owner or manager
-    //     // owner
-    //     await PresaleInstance.manualTransfer(accounts[1], 1000, { from: accounts[0] })
-    //
-    //     let tokenBalance = await TokenInstance.balanceOf(accounts[1]);
-    //     tokenBalance = tokenBalance.toNumber();
-    //     console.log(tokenBalance)
-    //
-    //     try {
-    //         // can be called only from owner of Presale
-    //         await PresaleInstance.setManager(accounts[1], { from: accounts[1] })
-    //         assert.fail()
-    //     } catch (error) {
-    //         err = error
-    //     }
-    //     assert.ok(err instanceof Error)
-    //     console.log(err)
-    //
-    //     await PresaleInstance.setManager(accounts[1],{ from: accounts[0] })
-    //
-    //     // manual transfer can be called only from owner or manager
-    //     // manager
-    //
-    //     // send to yourself should work correct
-    //     await PresaleInstance.manualTransfer(accounts[1], 1000, { from: accounts[1] })
-    //
-    //     tokenBalance = await TokenInstance.balanceOf(accounts[1]);
-    //     tokenBalance = tokenBalance.toNumber();
-    //     console.log(tokenBalance)
-    //
-    //     // send to another contract should work correct
-    //     // from manager
-    //     await PresaleInstance.manualTransfer(accounts[2], 1000, { from: accounts[1] })
-    //
-    //     tokenBalance = await TokenInstance.balanceOf(accounts[2]);
-    //     tokenBalance = tokenBalance.toNumber();
-    //     console.log(tokenBalance)
-    //
-    //
-    //     try {
-    //         // from another contract
-    //         await PresaleInstance.sendTransaction({ value: 1e+18, from: accounts[2] })
-    //         assert.fail()
-    //     } catch (error) {
-    //         err = error
-    //     }
-    //     assert.ok(err instanceof Error)
-    //     console.log(err)
-    //
-    // })
+        it("token's admin should be PreSale Contract", async () => {
+            let tokenInstance = await TokenInterface.deployed();
+            let preSaleInstance = await PreSaleInterface.deployed();
 
-})
+            // unnecessary because add PreSale to admin in migrations
+            // await tokenInstance.addAdmin(preSaleInstance.address);
+
+            let isAdmin = await tokenInstance.isAdmin(preSaleInstance.address);
+            console.log(isAdmin);
+            assert.equal(true, isAdmin);
+        });
+
+    });
+
+    describe('valid end of PreSale', function () {
+
+        it('should be ended only after end', async function () {
+            let ended = await this.preSale.hasEnded();
+            assert.equal(ended, false);
+            await increaseTimeTo(this.afterClosingTime);
+            ended = await this.preSale.hasEnded();
+            assert.equal(ended, true);
+        });
+
+    });
+
+    describe('creating a valid PreSale', function () {
+
+        it('should fail with zero period', async function () {
+            await PreSaleInterface.new(
+                this.startTime, 0, wallet, this.token.address, this.minimumInvest, {from: owner}
+            ).should.be.rejectedWith('revert');
+        });
+
+        it('should fail with zero token address', async function () {
+            await PreSaleInterface.new(
+                this.startTime, this.period, wallet, 0, this.minimumInvest, {from: owner}
+            ).should.be.rejectedWith('revert');
+        });
+
+    });
+
+    describe('accepting payments', function () {
+        it('should reject payments before start', async function () {
+            await this.preSale.sendTransaction({ value: value, from: investor }).should.be.rejectedWith(EVMRevert);
+        });
+
+        it('should accept payments after start', async function () {
+            await increaseTimeTo(this.openingTime);
+            await this.preSale.sendTransaction({ value: value, from: investor }).should.be.fulfilled;
+        });
+
+        it('should reject payments after end', async function () {
+            await increaseTimeTo(this.afterClosingTime);
+            await this.preSale.sendTransaction({ value: value, from: investor }).should.be.rejectedWith(EVMRevert);
+        });
+    });
+
+    describe('payable method', function () {
+
+        it('should fail with buy tokens from zero address', async function () {
+            this.preSale.sendTransaction({value: value, from: 0}).should.be.rejectedWith(EVMRevert);
+        });
+
+        it('should fail with value < minimumInvest', async function () {
+            this.preSale.sendTransaction({value: 10, from: investor}).should.be.rejectedWith(EVMRevert);
+        });
+
+        it('should pass with value minimumInvest', async function () {
+            this.preSale.sendTransaction({ value: this.minimumInvest, from: investor }).should.be.fulfilled;
+        });
+    });
+
+    describe('finish preSale method', function () {
+
+        it('should fail with wei not raised', async function () {
+            this.preSale.finishPreSale().should.be.rejectedWith('revert');
+        });
+
+        it('should pass with wei raised and time reached end time', async function () {
+            await this.preSale.sendTransaction({ value: value, from: investor });
+            await increaseTimeTo(this.afterClosingTime );
+            await this.preSale.finishPreSale().should.be.fulfilled;
+        });
+
+    });
+
+    describe('refund method', function () {
+
+        it('should fail with reach end time for preSale', async function () {
+            this.preSale.refund().should.be.rejectedWith('revert');
+        });
+
+        it('should pass with increasing time to endTime', async function () {
+            await this.preSale.sendTransaction({ value: minimumInvest, from: owner });
+            await increaseTimeTo(this.afterClosingTime );
+            await this.preSale.refund();
+        });
+
+    });
+
+});
